8798336373260ed2c324c6de97234124f52b0085
zainkhokhar1 <muhammadzain7113@gmail.com>
2025-09-25 17:27:50 +0500

    Completed header 90%

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import SearchInput from "../header/SearchInput.jsx";
 import Notifications from "../header/Notifications.jsx";
+import { ChevronDown } from "lucide-react";
 
 const Header = () => {
-    return <div className="flex items-center justify-between px-4 pt-3 pb-4 border border-black/5 border-l-0">
+    return <div className="flex items-center justify-between px-4 pt-2 pb-3 border border-black/5 border-l-0">
         <SearchInput />
 
         <div className="flex items-center gap-3">
-            <Notifications />
-            <span className="min-h-full bg-black/15 w-px">&nbsp;</span>
-        </div>
 
+            <div className="flex items-center gap-3">
+                <Notifications />
+                <span className="min-h-full bg-black/15 w-px">&nbsp;</span>
+            </div>
+
+            <div className="flex items-center gap-2 w-36 cursor-pointer pr-2 py-1 hover:bg-black/3 rounded-full duration-200">
+                {/* user profile */}
+                <div className="flex items-center gap-1 ">
+                    <div className="avatar avatar-online">
+                        <div className="w-9 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/gordon@192.webp" />
+                        </div>
+                    </div>
+                </div>
+                {/* show name */}
+                <h2 className="text-black/80 line-clamp-1 break-all w-full text-sm">
+                    Zain ul abideen
+                </h2>
+                <ChevronDown className="text-black/80" size={36} />
+            </div>
+
+        </div>
     </div>;
 };
 
--- frontend/src/components/header/Notifications.jsx
+++ frontend/src/components/header/Notifications.jsx
@@ -2,7 +2,7 @@ import { Bell } from "lucide-react";
 import React from "react";
 
 const Notifications = () => {
-    return <div className="p-2 shadow-xs border border-black/20 rounded-lg">
+    return <div className="p-2 shadow-xs hover:shadow-md duration-200 cursor-pointer border border-black/20 rounded-lg">
         <Bell size={18} className="text-black/80" />
     </div>;
 };
